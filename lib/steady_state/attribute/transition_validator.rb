31f0931d254f263247a5e6324c791b65b32b0aaa
Corey Alexander <coreyja@gmail.com>
2020-07-04 14:29:49 -0400

    Improve error messages with a from and to attributes, so errors won't always group together

--- lib/steady_state/attribute/transition_validator.rb
+++ lib/steady_state/attribute/transition_validator.rb
@@ -1,8 +1,10 @@
 module SteadyState
   module Attribute
     class TransitionValidator < ActiveModel::EachValidator
-      def validate_each(obj, attr_name, _value)
-        obj.errors.add(attr_name, :invalid) if obj.instance_variable_defined?("@last_valid_#{attr_name}")
+      def validate_each(obj, attr_name, value)
+        return unless obj.instance_variable_defined?("@last_valid_#{attr_name}")
+
+        obj.errors.add(attr_name, :invalid, from: obj.instance_variable_get("@last_valid_#{attr_name}"), to: value)
       end
     end
   end
